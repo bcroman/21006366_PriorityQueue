7ba2dd32b77fb3c790cbc26143fc2d2a13e5af43
bcroman <bencollins315@gmail.com>
2025-03-18 11:03:17 +0000

    Tested Nunit
    Create a Test testFunction to make sure the Nunit is working

--- PriorityQueue.Tests/Class1.cs
+++ PriorityQueue.Tests/Class1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using PriorityQueue;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace PriorityQueue.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        private HeapPriorityQueue<string> heapQueue;
+        private SortedArrayPriorityQueue<string> sortedArrayQueue;
+        private UnsortedArrayPriorityQueue<string> unsortedArrayQueue;
+        private SortedLinkedPriorityQueue<string> sortedLinkedQueue;
+        private UnsortedLinkedPriorityQueue<string> unsortedLinkedQueue;
+
+        [SetUp]
+        public void Setup()
+        {
+            heapQueue = new HeapPriorityQueue<string>(10);
+            sortedArrayQueue = new SortedArrayPriorityQueue<string>(10);
+            unsortedArrayQueue = new UnsortedArrayPriorityQueue<string>(10);
+            sortedLinkedQueue = new SortedLinkedPriorityQueue<string>(10);
+            unsortedLinkedQueue = new UnsortedLinkedPriorityQueue<string>(10);
+        }
+
+        [Test]
+        public void Add_And_Head_ReturnsHighestPriorityItem()
+        {
+            heapQueue.Add("Task A", 2);
+            heapQueue.Add("Task B", 5);
+            heapQueue.Add("Task C", 3);
+
+            // Act
+            string result = heapQueue.Head();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Task B"));
+        }
     }
 }
