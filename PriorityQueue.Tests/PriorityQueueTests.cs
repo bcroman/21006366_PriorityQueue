89e62421f43b33b0b065983ef2395417b13f0e01
bcroman <bencollins315@gmail.com>
2025-03-21 11:23:35 +0000

    Version 1
    Added the last comments to the project

--- PriorityQueue.Tests/PriorityQueueTests.cs
+++ PriorityQueue.Tests/PriorityQueueTests.cs
@@ -5,10 +5,13 @@ using System.Collections.Generic;
 
 namespace PriorityQueue.Tests
 {
+    /// <summary>
+    ///  Tests Scrpt that checks each function and error handling for each queue method
+    /// </summary>
     [TestFixture]
     public class PriorityQueueTests
     {
-        // List of queue implementations to be tested
+        // Create objects for all 5 queues
         private static IEnumerable<PriorityQueue<string>> GetQueues()
         {
             yield return new HeapPriorityQueue<string>(10);
@@ -18,67 +21,83 @@ namespace PriorityQueue.Tests
             yield return new UnsortedLinkedPriorityQueue<string>(10);
         }
 
+        //This tests the queue  by finding the highest proioruty item, by calling the head method
         [Test, TestCaseSource(nameof(GetQueues))]
         public void Add_And_Head_ReturnsHighestPriorityItem(PriorityQueue<string> queue)
         {
+            //Input Data
             queue.Add("Task A", 2);
             queue.Add("Task B", 5);
             queue.Add("Task C", 3);
 
-            // Act
+            //Call Head Method and Set to Result value
             string result = queue.Head();
 
-            // Assert
+            //Checks if the output is 'Task B'
             Assert.That(result, Is.EqualTo("Task B"));
         }
 
+        //This tests removes the root item and checks if the head is updated
         [Test, TestCaseSource(nameof(GetQueues))]
         public void Remove_UpdatesHeadProperly(PriorityQueue<string> queue)
         {
+            //Input Data
             queue.Add("Task A", 2);
             queue.Add("Task B", 5);
             queue.Add("Task C", 3);
 
+            //Call Remove Methed
             queue.Remove();
-            // Act
+            
+            //Call Head Method and Set to Result value
             string result = queue.Head();
 
-            // Assert
+            //Checks if the output is 'Task C'
             Assert.That(result, Is.EqualTo("Task C"));
         }
 
+        //Tests if the queue is empty
         [Test, TestCaseSource(nameof(GetQueues))]
         public void IsEmpty_ReturnsTrue_WhenQueueIsEmpty(PriorityQueue<string> queue)
         {
+            //Calls isEmpty Methiods and checks if output is True
             Assert.That(queue.IsEmpty(), Is.True);
         }
 
+        //Tests if the queue is empty with Add Data
         [Test, TestCaseSource(nameof(GetQueues))]
         public void IsEmpty_ReturnsFalse_WhenQueueHasItems(PriorityQueue<string> queue)
         {
-            queue.Add("Task A", 2);
-            Assert.That(queue.IsEmpty(), Is.False);
+            queue.Add("Task A", 2); //Input Data
+
+            Assert.That(queue.IsEmpty(), Is.False); //Calls isEmpty Methiods and checks if output is False
         }
 
+        //Checks if removes methond display error if queue is empty
         [Test, TestCaseSource(nameof(GetQueues))]
         public void Remove_ThrowsException_WhenQueueIsEmpty(PriorityQueue<string> queue)
         {
-            Assert.Throws<QueueUnderflowException>(() => queue.Remove());
+            Assert.Throws<QueueUnderflowException>(() => queue.Remove()); //Calls and Check Remove Methond
         }
 
+        //Checks if Queue is Full
         [Test, TestCaseSource(nameof(GetQueues))]
         public void Add_ThrowsException_WhenQueueIsFull(PriorityQueue<string> queue)
         {
+            //Checks if each queue has a fix size
             if (queue is HeapPriorityQueue<string> ||
                 queue is SortedArrayPriorityQueue<string> ||
                 queue is UnsortedArrayPriorityQueue<string> ||
                 queue is SortedLinkedPriorityQueue<string> ||
                 queue is UnsortedLinkedPriorityQueue<string>)
             {
-                var smallQueue = new HeapPriorityQueue<string>(2);
+                var smallQueue = new HeapPriorityQueue<string>(2); //Updates size to 2 items
+
+                //Input Data
                 smallQueue.Add("Task A", 1);
                 smallQueue.Add("Task B", 2);
 
+                //Trys to Add another item to queue by gets a error message.
                 Assert.Throws<QueueOverflowException>(() => smallQueue.Add("Task C", 3));
             }
         }
--- PriorityQueue/SortedArrayPriorityQueue.cs
+++ PriorityQueue/SortedArrayPriorityQueue.cs
@@ -2,12 +2,20 @@
 
 namespace PriorityQueue
 {
+    /// <summary>
+    /// Represents a priority quene using a sorted array
+    /// </summary>
     public class SortedArrayPriorityQueue<T> : PriorityQueue<T>
     {
+        //Variables
         private readonly PriorityItem<T>[] storage;
         private readonly int capacity;
         private int tailIndex;
 
+        /// <summary>
+        /// Initializes a new instance of the SortedArrayPriorityQueue class.
+        /// </summary>
+        /// <param name="size">The maximum number of elements the queue can hold.</param>
         public SortedArrayPriorityQueue(int size)
         {
             storage = new PriorityItem<T>[size];
@@ -15,6 +23,11 @@ namespace PriorityQueue
             tailIndex = -1;
         }
 
+        /// <summary>
+        /// Returns the item with the highest priority
+        /// </summary>
+        /// <returns>The item with the highest priority.</returns>
+        /// <exception cref="QueueUnderflowException">Thrown if the queue is empty.</exception>
         public T Head()
         {
             if (IsEmpty())
@@ -24,6 +37,12 @@ namespace PriorityQueue
             return storage[0].Item;
         }
 
+        /// <summary>
+        /// Adds Item to queue and finds the right position for it
+        /// </summary>
+        /// <param name="item">The item to be added</param>
+        /// <param name="priority">The priority of the ite</param>
+        /// <exception cref="QueueOverflowException">Thrown if the queue is full.</exception>
         public void Add(T item, int priority)
         {
             tailIndex++;
@@ -42,6 +61,10 @@ namespace PriorityQueue
             storage[i] = new PriorityItem<T>(item, priority);
         }
 
+        /// <summary>
+        /// Removes Item with highest priority of the queue
+        /// </summary>
+        /// <exception cref="QueueUnderflowException">Thrown if the queue is empty.</exception>
         public void Remove()
         {
             if (IsEmpty())
@@ -56,11 +79,19 @@ namespace PriorityQueue
             tailIndex--;
         }
 
+        /// <summary>
+        /// Check if queue has items or not.
+        /// </summary>
+        /// <returns>True or false base if queue is empty</returns>
         public bool IsEmpty()
         {
             return tailIndex < 0;
         }
 
+        /// <summary>
+        /// Display Array Items
+        /// </summary>
+        /// <returns>Display a string value of array values</returns>
         public override string ToString()
         {
             if (IsEmpty())
